0e5d42b999324aff83dd5beadbc729c161af5938
Roman Rodin <romaro83@gmail.com>
2022-06-19 11:46:19 +0300

    props

---
+++ src/Props.tsx
@@ -0,0 +1,7 @@
+export function Props() {
+    return (
+        <div id="testRoot">
+            <h1>Hello!</h1>
+        </div>
+    )
+}
--- src/Simple.tsx
+++ src/Simple.tsx
@@ -1,5 +1,3 @@
-//import React from 'react';
-
 export function Test() {
     return (
         <div id="testRoot">
