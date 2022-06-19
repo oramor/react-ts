64fb014a3be9f94bc2e283e684f2d8e7f453dfcf
Roman Rodin <romaro83@gmail.com>
2022-06-19 06:20:54 +0300

    test

---
+++ dist/Test.js
@@ -0,0 +1,5 @@
+"use strict";
+//import React from 'react';
+function Test() {
+    return (_jsx("div", Object.assign({ id: "testRoot" }, { children: _jsx("h1", { children: "Hello!" }) })));
+}
---
+++ src/Test.tsx
@@ -0,0 +1,9 @@
+//import React from 'react';
+
+function Test() {
+    return (
+        <div id="testRoot">
+            <h1>Hello!</h1>
+        </div>
+    )
+}
